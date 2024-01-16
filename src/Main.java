f3c37a75c022b42c8c34dbb8ac1cdf33d0d5501c
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2024-01-16 07:54:46 -0500

    suma de dos numeros

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        JFrame frame = new JFrame("Suma de dos numeros");
+
+        //lo que va a cargar en el contenido sera dentro de form1
+        frame.setContentPane(new form1().menuPrincipal);
+
+        //CUANDO DE CLICK EN LA x SE CIERRE EL PROGRAMA
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.pack();
+        //para que aparesca el recuadro
+        frame.setVisible(true);
     }
 }
--- src/form1.java
+++ src/form1.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class form1 {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class form1 {
+    private JButton OKButton;
+    private JLabel numero1Lbl;
+    private JTextField numero1textField1;
+    private JTextField numero2textField2;
+    private JLabel numer2Lbl;
+    public JPanel menuPrincipal;
+    private JLabel numero3JLbl;
+
+    //opcion create listener
+
+    public form1() {
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hola a todos");
+                String n1;
+                n1=numero1textField1.getText();
+                String n2;
+                n2=numero2textField2.getText();
+                //operacion de suma
+                int numero1, numero2, resultado;
+                numero1=Integer.parseInt(n1);
+                numero2=Integer.parseInt(n2);
+                resultado=numero1+numero2;
+                System.out.println(n1);
+                System.out.println(n2);
+                System.out.println(resultado);
+
+                //settear los resultados de int a string
+                String r3;
+                r3=Integer.toString(resultado);
+                numero3JLbl.setText("Resultado: "+r3);
+            }
+        });
+    }
 }
